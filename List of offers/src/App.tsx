2dd93fdc7f5c012ad56219988a1c46359ae5f3f8
Andrey Maximov <arman.gen40@mail.ru>
2024-11-04 20:22:38 +0300

    Json parse

--- List of offers/src/App.tsx
+++ List of offers/src/App.tsx
@@ -1,10 +1,9 @@
 import data from './data/etsy.json'
 import { Listing }from './components/Listing/Listing';
 import './App.css'
-import { IItem } from './models';
 
 function App() {
-  const dataProps: IItem[] = data
+  const dataProps = JSON.parse(data.toString())
   return (
     <Listing items={dataProps}></Listing>
   )
